9a233c6b092c6320fb57ebc6d702a64efea66cf0
Roberto Stagg <roberto@Robertos-MacBook-Pro.local>
2023-12-05 18:19:20 -0800

    Enhanced Spring Boot Gateway with CORS Configuration and Observed Backend Modifications
    - Implemented CorsWebFilter in the Gateway Service Config to manage Cross-Origin Resource Sharing (CORS), allowing seamless communication with the Angular frontend.
    - Configured CORS to accept requests from 'http://localhost:4200', supporting all HTTP methods and headers, and applying this configuration to all paths.
    - Removed the @CrossOrigin annotation from the Booking Service controller, centralizing CORS management in the Gateway Service.
    - Noticed modifications in the target folders of the Price Service and Eureka Server. These changes are currently under observation, with all backend functionalities appearing to operate as expected.
    
    This update ensures a more centralized and efficient handling of CORS, contributing to the stability and security of the backend architecture.
    
    Next Steps: Monitor the backend services for any unexpected behavior due to the recent modifications and continue with frontend development and testing.

--- GatewayService/src/main/java/com/microservices/gatewayservice/config/GatewayConfig.java
+++ GatewayService/src/main/java/com/microservices/gatewayservice/config/GatewayConfig.java
@@ -1,9 +1,14 @@
 package com.microservices.gatewayservice.config;
 
+import java.util.Arrays;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class GatewayConfig {
@@ -11,10 +16,24 @@ public class GatewayConfig {
 	@Bean
 	RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
 		
-		return builder.routes().route(p -> p.path("/bookings")
+		return builder.routes().route(p -> p.path("/bookings/**")
 									   		.uri("lb://booking-service"))
 							   .build();
 	}
+	
+	@Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Set the allowed origins
+        corsConfig.setMaxAge(3600L); // Set how long the response from a pre-flight request can be cached by clients
+        corsConfig.addAllowedMethod("*"); // Allow all HTTP methods
+        corsConfig.addAllowedHeader("*"); // Allow all headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig); // Apply CORS configuration to all paths
+
+        return new CorsWebFilter(source); // Create and return the CORS filter
+    }
 }
 
 /*
